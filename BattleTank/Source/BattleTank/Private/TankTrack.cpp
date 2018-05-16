e1e9fd4a06b6e18fefdbee495588ac4209b41d5f
Unknown <graiiner@gmail.com>
2018-05-16 15:59:27 +0200

    Setting Tracks' Throttles (part 1)

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/TankTrack.h"
+#include "BattleTank/BattleTank.h"
+#include "Engine/World.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)
+
+	// TODO Clamp actual throttle value se player can't overdrive 
+}
+
