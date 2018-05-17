508b69b7547e149db0c2d59d60fb499de87005d3
Unknown <graiiner@gmail.com>
2018-05-17 18:58:03 +0200

    Setting Tanks' Movement (part 2)
    Setting movement forward

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,11 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Public/TankMovementComponent.h"
+#include "BattleTank/BattleTank.h"
+#include "Public/TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double speeding due to controllers dual shoulder buttons
 }
 
 
